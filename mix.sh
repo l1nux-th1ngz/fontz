1cb427d1bdf45790c4763b98b6866f8acde966e8
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-25 00:03:30 -0700

    Update mix.sh

--- mix.sh
+++ mix.sh
@@ -5,5 +5,8 @@ sudo pacman -Syu
 
 # Install the Cascadia Code fonts
 yay -S ttf-cascadia-code ttf-ms-fonts ttf-ms-win10-auto
-ttf-ms-win11 ttf-vista-fonts ttc-iosevka
- 
+ttf-ms-win11 ttf-vista-fonts ttc-iosevka droid_sans.ttf
+icomoon_feather.ttf material.ttf noto_sans.ttf
+ adobe-source-sans-fonts adobe-source-serif-fonts
+adobe-source-code-pro-fonts noto-fonts noto-fonts-emoji
+noto-fonts-extra texlive-fontsextra
