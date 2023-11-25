c4004caa2f70f6c69796a0d77618ce31b1ce772d
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-24 23:15:43 -0700

    Create ms.sh

---
+++ ms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update the package database
+sudo pacman -Syu
+
+# Install the Cascadia Code fonts
+yay -S ttf-cascadia-code ttf-ms-fonts ttf-ms-win10-auto
+ttf-ms-win11 ttf-vista-fonts
+ 
