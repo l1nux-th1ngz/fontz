3434dc03e598450876d9fb6be08687f98d96d7fa
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-24 23:42:19 -0700

    Update and rename ms.sh to mix.sh

--- mix.sh
+++ mix.sh
@@ -5,5 +5,5 @@ sudo pacman -Syu
 
 # Install the Cascadia Code fonts
 yay -S ttf-cascadia-code ttf-ms-fonts ttf-ms-win10-auto
-ttf-ms-win11 ttf-vista-fonts
+ttf-ms-win11 ttf-vista-fonts ttc-iosevka
  
